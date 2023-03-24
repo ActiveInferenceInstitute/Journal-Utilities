77c9a86158da961f9f5d55489d4f51139b829f18
TheButtskie <65842772+TheButtskie@users.noreply.github.com>
2023-03-24 14:33:20 -0400

    Add files via upload

---
+++ youtube_to_github/settings.py
@@ -0,0 +1,13 @@
+youtube_channel: str = 'https://www.youtube.com/@ActiveInference'
+
+github_token: str = ''
+''' 
+to get your github token navitage to the following link:
+
+https://github.com/settings/tokens
+
+and click generate token classic > and scope select repo  (to allow all of the repo items)
+The time perieod tehaeet eyou ewanet to us thee token will tell you how often you have to update the token in this script prior to running.
+
+the token generated will be the toke that you use in github_token
+'''
